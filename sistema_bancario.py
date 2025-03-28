dc0b0fcad559478c0683239e3cef8dfc0b988b2c
lyss <yasmin.menas.campos@escola.pr.gov.br>
2025-03-28 11:14:27 -0300

    criação da função MENU

---
+++ sistema_bancario.py
@@ -0,0 +1,18 @@
+#print("******** Menu ********")
+#print("Criar Usuário")
+#print("Depositar")
+#print("Sacar")
+
+def menu():
+
+    print('''
+     ******** Menu ********
+     (1) Criar Usuário
+     (2) Criar conta
+     (3) Listar Contas
+     (s) Sacar
+     (d) Depositar
+     (e) Extrato
+     (q) Sair
+    ''')
+    return input('Qual opção deseja')
